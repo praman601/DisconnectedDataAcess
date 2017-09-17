c48406fa6bbc68d517a84a91f81f76774011743b
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 11:35:20 +0545

    another commit

---
+++ disconnectedDAtaAccess/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="disconnectedDAtaAccess.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Get data" OnClick="Button1_Click" />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="undo" />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                    <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                    <asp:BoundField DataField="TotalMarks" HeaderText="TotalMarks" SortExpression="TotalMarks" />
+                </Columns>
+            </asp:GridView>
+        </div>
+        <asp:Button ID="Button2" runat="server" Text="Update database" OnClick="Button2_Click" />
+        
+        <asp:Label ID="Label1" Font-Bold="true" runat="server"></asp:Label>
+        </form>
+        
+</body>
+</html>
---
+++ disconnectedDAtaAccess/WebForm1.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace disconnectedDAtaAccess
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getDAtaFromDb()
+        {
+            SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial catalog=Students;Integrated Security=true");
+            String strSelectCmd = "select * from tblStudents";
+            SqlDataAdapter da = new SqlDataAdapter(strSelectCmd, con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Students");
+
+            ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["Id"] };
+            Cache.Insert("Dataset", ds, null, System.DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            Label1.Text = "Data Loaded from DataBase";
+
+        }
+
+        private void getDataFromCache()
+        {
+            if (Cache["Dataset"] != null)
+            {
+                DataSet ds = (DataSet)Cache["Dataset"];
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+
+
+
+            }
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getDAtaFromDb();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            getDataFromCache();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            if(Cache["Dataset"]!=null)
+            {
+                DataSet ds =  (DataSet)Cache["Dataset"];
+                DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["Id"]);
+
+               dr["Name"]= e.NewValues["Name"];
+                dr["Gender"] = e.NewValues["Gender"];
+                dr["TotalMarks"] = e.NewValues["TotalMarks"];
+
+                Cache.Insert("Dataset", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+                GridView1.EditIndex = -1;
+                getDataFromCache();
+
+            }
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex =-1;
+            getDataFromCache();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            if (Cache["Dataset"] != null)
+            {
+                DataSet ds = (DataSet)Cache["Dataset"];
+                DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["Id"]);
+
+                dr.Delete();
+
+                Cache.Insert("Dataset", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+               
+                getDataFromCache();
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial catalog=Students;Integrated Security=true");
+            String strSelectCmd = "select * from tblStudents";
+            SqlDataAdapter da = new SqlDataAdapter(strSelectCmd, con);
+
+            DataSet ds=(DataSet)Cache["Dataset"];
+
+            String strUpdateCmd = "update tblStudents set Name=@Name, Gender=@Gender, TotalMarks=@TotalMarks where Id=@Id";
+            SqlCommand updateCommand = new SqlCommand(strUpdateCmd, con);
+            updateCommand.Parameters.Add("@Name", SqlDbType.VarChar, 50, "Name");
+            updateCommand.Parameters.Add("@Gender", SqlDbType.VarChar, 10, "Gender");
+            updateCommand.Parameters.Add("@TotalMarks", SqlDbType.Int, 0, "TotalMarks");
+            updateCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+
+            da.UpdateCommand = updateCommand;
+
+            String strDeleteCmd = "delete from tblStudents where Id=@Id";
+            SqlCommand deleteCommand = new SqlCommand(strDeleteCmd, con);
+            deleteCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+
+            da.DeleteCommand = deleteCommand;
+
+            da.Update(ds, "Students");
+
+            Label1.Text = "Database Table Updated";
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DataSet ds = (DataSet)Cache["Dataset"];
+            if(ds.HasChanges())
+            {
+                ds.RejectChanges();
+                Cache.Insert("Dataset", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+                getDataFromCache();
+                Label1.Text = "Changes Undone";
+                Label1.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+
+                Label1.Text = "No changes made";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+
+
+
+
+            /* for status of each row
+            foreach(DataRow dr in ds.Tables["Students"].Rows)
+            {
+                if(dr.RowState==DataRowState.Deleted)
+                {
+                    Response.Write(dr["Id",DataRowVersion.Original].ToString() + "-" + dr.RowState.ToString() + "<br/>");
+                }
+
+                else{
+                    Response.Write(dr["Id"].ToString() + "-" + dr.RowState.ToString() + "<br/>");
+                }
+            }*/
+        }
+    }
+}
